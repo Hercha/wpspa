be0a7b13bb248dd407c764241758d5510374ea96
fedjensa <jens.hedberg@medieinstitutet.se>
2017-10-02 22:55:44 +0200

    showing posts

--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import nav from './globals/nav.vue'
+import footer from './globals/footer.vue'
+import PostExcerpt from './posts/post-excerpt.vue'
+
+Vue.component('app-main-nav', nav );
+Vue.component('app-footer', footer);
+Vue.component('app-post-excerpt', PostExcerpt);
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+  data: {
+
+  }
+});
