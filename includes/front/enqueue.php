1125047ab21717c3881117a1c407fa2f452188da
fedjensa <jens.hedberg@medieinstitutet.se>
2017-10-02 21:30:04 +0200

    startup cont

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,5 +1,29 @@
 <?php
 
 function spa_enqueue_scripts(){
+    wp_register_style(
+        'spa_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css'
+    );
 
+    wp_enqueue_style('spa_bootstrap');
+
+    wp_register_script(
+        'spa_bootstrap',
+        get_template_directory_uri() . '/assets/js/bootstrap.min.js',
+        array(),
+        false,
+        true
+    );
+
+    wp_register_script(
+        'spa_bundle',
+        'http://localhost:8080/dist/build.js',
+        [],
+        false,
+        true
+    );
+
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('spa_bootstrapp');
+    wp_enqueue_script('spa_bundle');
 }
